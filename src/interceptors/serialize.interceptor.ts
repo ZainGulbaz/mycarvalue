7ed981c68a56f4898327148936a26c4a3aa710e5
Zain Gulbaz <zaingulbaz8@gmail.com>
2023-08-30 20:37:55 +0500

    Interceptor

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,33 +1,36 @@
-import { NestInterceptor,ExecutionContext,CallHandler,UseInterceptors } from "@nestjs/common";
+import { ExecutionContext,CallHandler, NestInterceptor } from "@nestjs/common/interfaces";
+import { UseInterceptors } from "@nestjs/common/decorators";
 import { map } from "rxjs/operators";
 import { Observable } from "rxjs";
-import { plainToClass } from "class-transformer";
+import { plainToClass, plainToInstance } from "class-transformer";
 import Response from "src/types/Response";
 
 
 export function Serialize(dtos){
-  return UseInterceptors(new SerializeInterceptor<typeof dtos>(dtos))
+  return UseInterceptors(new ResponseSerialization(dtos));
 }
 
 
+export class ResponseSerialization implements NestInterceptor{
 
-export class SerializeInterceptor<T> implements NestInterceptor{
+  constructor(public dtos:any){}
+  
+  intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
 
-    constructor(public dtos:T){}
+   return next.handle().pipe(map((responsePayload:Response)=>{
+    const responseData=responsePayload.data;
+    let finalData={};
+    Object.keys(responseData)?.map((entity)=>{
 
-    intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
-       return next.handle().pipe(map((responsePayload:Response)=>{
-       let finalData={};
-       Object.keys(responsePayload.data)?.map(entity=>{
-
-        const plainClass= plainToClass(this.dtos[entity],responsePayload.data[entity],{
-            excludeExtraneousValues:true
-       })
-       finalData={...finalData,[entity]:plainClass}
-       }); 
-       
-        return {...responsePayload,data:finalData};
-       }));
-    }
-
+      const refinedData= plainToInstance(this.dtos[entity],responsePayload.data[entity],{
+        excludeExtraneousValues:true
+      });
+      finalData={...finalData,[entity]:refinedData};
+    })   
+  
+    responsePayload.data=finalData;
+    return responsePayload;
+                
+  }))
+}
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,13 @@
-import { Controller,Post,Get,Put,Delete,Param,Query,Body,Res} from '@nestjs/common';
+import { Controller,Post,Get,Put,Delete,Param,Query,Body,Res,UseInterceptors} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/user-create.dto';
 import { GetUserByIdDto } from './dtos/user-id.dto';
 import { GetUsersByEmail } from './dtos/users.dto';
 import { UpdateUserDto } from './dtos/user-update.dto';
+import { ResponseSerialization, Serialize } from 'src/interceptors/serialize.interceptor';
 import {Response} from "express";
-import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserInterceptorDto } from './dtos/user-interceptor.dto';
+
 
 @Controller()
 export class UsersController {
@@ -25,6 +26,7 @@ export class UsersController {
          return await this.userService.findOne(params.id,res);
     }
     
+
     @Serialize({users:UserInterceptorDto})
     @Get("user")
     async getUsers(@Query() queryParams:GetUsersByEmail, @Res({passthrough:true}) res:Response){
